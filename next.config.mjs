785d6a9373e2135fcf8b4376be8ae007bc2afc7c
Chirag Agarwal <chiragagarwal819@gmail.com>
2025-04-05 11:18:34 +0530

    second commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    trailingSlash: true, // optional but often helpful for static hosting
+  };
+  
+  export default nextConfig;
+  
